49cfb4b63ea19012891e636b4902d3a864c2fb8f
unknown <FP0023@FoxwellPower.Local>
2023-05-09 11:53:50 +0800

    使用 Link 建立 Header 中的 navs

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const HeaderWrapper = styled.header`
     display: flex;
@@ -7,6 +8,25 @@ const HeaderWrapper = styled.header`
     align-items: center;
     width: 100%;
     border: 5px solid black;
+`
+
+const LinksWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    a {
+        margin: 0 5px;
+        color: navy;
+        font-size: 18px;
+        font-weight: 600;
+        text-decoration: none;
+        cursor: pointer;
+
+        :hover {
+            text-decoration: underline;
+        }
+    }
 `
 
 export default function Header() {
@@ -18,7 +38,10 @@ export default function Header() {
                 height={64} // Desired size with correct aspect ratio
                 alt="Logo"
             />
-            <h1>This is the Header.</h1>
+            <LinksWrapper>
+                <Link href={'/'}>首頁</Link>
+                <Link href={'/products'}>products</Link>
+            </LinksWrapper>
         </HeaderWrapper>
     )
 }
